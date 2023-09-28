58800706c974d8a1fcc71391d3efff33afb66530
Rafael Mota Moraes <rafaelmota6622@gmail.com>
2023-09-28 11:31:23 -0300

    criando usuários

---
+++ backend/src/models/User.js
@@ -0,0 +1,63 @@
+import Sequelize, { Model } from 'sequelize';
+import bcryptjs from 'bcryptjs';
+
+export default class User extends Model {
+  static init(sequelize) {
+    super.init({
+      nome: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [3, 255],
+            msg: 'Campo nome precisa ter entre 3 e 255 caracteres',
+          },
+        },
+      },
+      email: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          isEmail: {
+            msg: 'Email inválido',
+          },
+
+          // Tive que fazer isso para funcionar
+          // não sei dizer o motivo
+          isUnique(value, next) {
+            User.findOne({ where: { email: value } })
+              .then((user) => {
+                if (user) {
+                  return next('E-mail já existe, tente outro!');
+                }
+                return next();
+              })
+              .catch((err) => next(err));
+          },
+        },
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+      },
+      password: {
+        type: Sequelize.VIRTUAL,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [6, 50],
+            msg: 'Campo password precisa ter entre 6 e 50 caracteres',
+          },
+        },
+      },
+    }, {
+      sequelize,
+    });
+
+    this.addHook('beforeSave', async (user) => {
+      user.password_hash = await bcryptjs.hash(user.password, 8);
+    });
+
+    return this;
+  }
+}
