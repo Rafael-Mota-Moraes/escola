20cde3ad2642fe93fb91bc6c799383a9b5797822
Rafael Mota Moraes <rafaelmota6622@gmail.com>
2023-09-28 13:14:43 -0300

    CRUD de usuários

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -4,9 +4,84 @@ class UserController {
   async store(req, res) {
     try {
       const novoUser = await User.create(req.body);
-      res.json({ user: novoUser });
+      return res.json({ user: novoUser });
     } catch (e) {
-      res.status(400).json({ errors: e.errors.map((err) => err.message) });
+      return res.status(400).json({ errors: e.errors.map((err) => err.message) });
+    }
+  }
+
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+      return res.json(users);
+    } catch (e) {
+      return res.status(400).json({ errors: e.errors.map((err) => err.message) });
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+      const user = await User.findByPk(id);
+      return res.json(user);
+    } catch (e) {
+      return res.status(400).json({ errors: e.errors.map((err) => err.message) });
+    }
+  }
+
+  async update(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors:
+          ['ID não enviado.'],
+        });
+      }
+
+      const { id } = req.params;
+      const user = await User.findByPk(id);
+
+      if (!user) {
+        return res.status(400).json({
+          errors:
+          ['Usuário não existe'],
+        });
+      }
+
+      const novosDados = await user.update(req.body);
+
+      return res.json(novosDados);
+    } catch (e) {
+      console.log(e);
+      return res.status(400).json({ errors: e.errors?.map((err) => err.message) });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors:
+          ['ID não enviado.'],
+        });
+      }
+
+      const { id } = req.params;
+      const user = await User.findByPk(id);
+
+      if (!user) {
+        return res.status(400).json({
+          errors:
+          ['Usuário não existe'],
+        });
+      }
+
+      await user.destroy();
+
+      return res.json(user);
+    } catch (e) {
+      console.log(e);
+      return res.status(400).json({ errors: e.errors?.map((err) => err.message) });
     }
   }
 }
--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -4,5 +4,9 @@ import userController from '../controllers/UserController';
 const router = Router();
 
 router.post('/', userController.store);
+router.get('/', userController.index);
+router.get('/:id', userController.show);
+router.put('/:id', userController.update);
+router.delete('/:id', userController.delete);
 
 export default router;
