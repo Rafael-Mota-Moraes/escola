d68bfc0bf21e5cb3ed7e11a7a596f2fefbcfa4b1
Rafael Mota Moraes <rafaelmota622@gmail.com>
2023-10-02 11:17:35 -0300

    front end e styled components

---
+++ frontend/src/pages/Login/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Paragrafo, Title } from "./styled";
+import { Container } from "../../styles/GlobalStyles";
+
+export default function Login() {
+  return (
+    <Container>
+      <Title isRed={false}>
+        Login <small>Olá</small>
+      </Title>
+      <Paragrafo>Lorem ipsum dolor sit amet.</Paragrafo>
+    </Container>
+  );
+}
