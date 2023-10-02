7fb3b4f6ba7a0041fa2fda69115af5dbd2465d31
Rafael Mota Moraes <rafaelmota622@gmail.com>
2023-10-02 15:34:55 -0300

    Menu da aplicação

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
+import Header from "./components/Header";
 import Login from "./pages/Login";
 import GlobalStyle from "./styles/GlobalStyles";
 
 function App() {
   return (
     <>
+      <Header />
       <GlobalStyle />
       <Login />
     </>
