2fd2458468a7c4c905d5b1ac9401f8380d6d84ef
Rafael Mota Moraes <rafaelmota622@gmail.com>
2023-10-06 11:58:16 -0300

    Cadastrando novos usuários na aplicação

--- frontend/src/pages/Register/index.jsx
+++ frontend/src/pages/Register/index.jsx
@@ -1,11 +1,95 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { toast } from "react-toastify";
+import isEmail from "validator/lib/isEmail";
 import { Container } from "../../styles/GlobalStyles";
+import { Form } from "./styled";
+import axios from "../../services/axios";
+import history from "../../services/history";
+import { get } from "lodash";
 
 export default function Register() {
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    let formErrors = false;
+
+    if (nome.length < 3 || nome.length > 255) {
+      formErrors = true;
+      toast.error("Campo nome precisa ter entre 3 e 255 caracteres");
+    }
+
+    if (!isEmail(email)) {
+      formErrors = true;
+      toast.error("E-mail inválido");
+    }
+
+    if (password.length < 6 || password.length > 50) {
+      formErrors = true;
+      toast.error("Campo senha precisa ter entre 6 e 50 caracteres");
+    }
+
+    if (formErrors) return;
+
+    try {
+      await axios.post("/users", {
+        nome: nome,
+        password: password,
+        email: email,
+      });
+
+      toast.success("Usuário criado.");
+      history.push("/login");
+    } catch (error) {
+      const status = get(error, "response.status", 0);
+      const errors = get(error, "response.data.errors", "");
+
+      errors.map((e) => toast.error(e));
+    }
+  }
+
   return (
     <Container>
-      <h1>Register</h1>
+      <h1>Crie sua conta</h1>
+
+      <Form onSubmit={handleSubmit}>
+        <label htmlFor="nome">
+          Nome
+          <input
+            type="text"
+            id="nome"
+            value={nome}
+            onChange={(e) => setNome(e.target.value)}
+            placeholder="Seu nome"
+          />
+        </label>
+
+        <label htmlFor="email">
+          E-mail
+          <input
+            type="text"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Seu e-mail"
+          />
+        </label>
+
+        <label htmlFor="senha">
+          Senha
+          <input
+            type="text"
+            id="senha"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Sua senha"
+          />
+        </label>
+
+        <button type="submit">Criar Minha Conta</button>
+      </Form>
     </Container>
   );
 }
--- frontend/src/store/modules/reduxPersist.js
+++ frontend/src/store/modules/reduxPersist.js
@@ -4,7 +4,7 @@ import { persistReducer } from "redux-persist";
 export default (reducers) => {
   const persistedReducers = persistReducer(
     {
-      key: "escola",
+      key: "REACT-BASE",
       storage,
       whitelist: ["example"],
     },
